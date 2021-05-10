46c67258b5b0de7f67c76288a1fdacc9410504c4
Akshive <pandeyakshive97@gmail.com>
2021-05-10 22:30:15 +0530

    complete

--- client/client.jsx
+++ client/client.jsx
@@ -1,5 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from './App'
+import { App } from "./App";
+import {handleModifyANswerVots} from '../shared/utility'
+// ReactDOM.render(<App />, document.getElementById("container"));
 
-ReactDOM.render(<App />, document.getElementById("container"));
+let state = undefined;
+fetch("http://localhost:7777/data")
+  .then((data) => data.json())
+  .then((json) => {
+    state = json;
+    console.log("Got the state", state);
+    render();
+  });
+
+function handleVots(answerId, increment) {
+  state.answers = handleModifyANswerVots(state.answers, answerId, increment);
+  fetch(`vote/${answerId}?increment=${increment}`)
+  render();
+}
+
+function render() {
+    console.log(handleModifyANswerVots)
+  ReactDOM.hydrate(<App {...state} handleModifyANswerVots={handleVots}/>, document.getElementById("container"));
+}
--- server/index.js
+++ server/index.js
@@ -3,14 +3,46 @@ import { readFileSync } from "fs";
 import React from "react";
 import { App } from "../client/App";
 import { renderToString } from "react-dom/server";
+import { handleModifyANswerVots } from "../shared/utility";
+
+const data = {
+  questions: [
+    {
+      questionId: "Q1",
+      content: "Should we use Jquery or Fetch for Ajax",
+    },
+  ],
+  answers: [
+    {
+      answerId: "A1",
+      questionId: "Q1",
+      upvotes: 2,
+      content: "JQuery",
+    },
+  ],
+};
 
 const app = new express();
 
 app.use(express.static("dist"));
+app.get("/vote/:answerId", (req, res) => {
+  const { query, params } = req;
+  console.log(query, params);
+  data.answers = handleModifyANswerVots(
+    data.answers,
+    params.answerId,
+    +query.increment
+  );
+  res.send("OK");
+});
+
+app.get("/data", async (_req, res) => {
+  res.json(data);
+});
 
 app.get("/", async (_req, res) => {
   const index = readFileSync(`public/index.html`, `utf8`);
-  const rendered = renderToString(<App />);
+  const rendered = renderToString(<App {...data} />);
   res.send(index.replace("{{rendered}}", rendered));
 });
 
